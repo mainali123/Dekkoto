12d0dd324d3c1b4040666fd0592a82a2bb5ebc82
Diwash Mainali <diwashmainali900@gmail.com>
2024-03-15 07:30:43 +0545

    carousel fixed completely

--- ui/static/js/homepage.js
+++ ui/static/js/homepage.js
@@ -47,13 +47,13 @@ fetch('/caroselSlide', {
                 img.alt = item.Title;
                 img.dataset.videoDetails = JSON.stringify(item); // Store the video details in the img element
                 console.log(img.dataset.videoDetails)
-                // on click to the active carousel item, redirect to the watch video page
+                /*// on click to the active carousel item, redirect to the watch video page
                 carouselItem.addEventListener('click', function () {
                     console.log('Carousel item clicked'); // Add this line
                     // save the video details in local storage
                     localStorage.setItem('videoDetails', this.dataset.videoDetails);
                     window.location.href = '/watchVideo'; // Redirect to the watch video page
-                });
+                });*/
 
                 // Append the created HTML elements to the carousel item
                 carouselItem.appendChild(img);
@@ -66,6 +66,18 @@ fetch('/caroselSlide', {
     .catch((error) => {
         console.error('Error:', error);
     });
+
+const carouselContainer = document.querySelector('.carousel-inner'); // Select the HTML element where you want to display the carousel items
+
+carouselContainer.addEventListener('click', (event) => {
+    let activeElement = document.querySelector('.carousel-item.active img');
+    if (activeElement) {
+        let details = JSON.parse(activeElement.getAttribute('data-video-details'));
+        details = JSON.stringify(details);
+        localStorage.setItem('videoDetails', details);
+        window.location.href = '/watchVideo'; // Redirect to the watch video page
+    }
+});
 
 // Recently Added Videos
 fetch('/recentlyAdded', {
